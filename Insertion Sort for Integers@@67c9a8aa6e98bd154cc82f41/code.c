0d7cc4d371ffbde0ed37b285db8d2c6035e2bbb4
Gazal1116 <gazalsingla00011@gmail.com>
2025-03-28 21:07:36 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,8 +1,8 @@
 // Your code here...
 void selectionSort (int arr[],int n) {
-    for(i=0;i<n-1;i++) {
+    for( int i=0;i<n-1;i++) {
         int smallestindex=i;
-        for(j=i+1;j<n;j++) {
+        for( int j=i+1;j<n;j++) {
             if(arr[j] < arr[smallestIndex]) {
                 smallestIndex=j;
             }
